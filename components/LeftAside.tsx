77c94250b961378f1ce23c81886b15ba0b5bcfba
esn8der <ballesta.esneider@gmail.com>
2023-09-24 18:01:09 -0500

    🚧 ✅ Terminado el aside izquierdo y derecho

--- components/LeftAside.tsx
+++ components/LeftAside.tsx
@@ -1,95 +1,77 @@
 import Image from 'next/image'
 import { TbLayersIntersect } from 'react-icons/tb'
+import { ProgressBar } from './ProgressBar'
+import SkillsSection from './SkillsSection';
+
+const skillsList = [
+    'SpringBoot',
+    'React, Next.js',
+    'Git, Github',
+    'Logica de Programación',
+];
+
 const LefAside = () => {
     return (
-        <aside className="relative flex flex-col w-[305px] h-full bg-white text-text-primary border border-red-500 items-center">
-        <div className='flex flex-col h-fit mx-[74px] mt-[50px] mb-[60px] text-center items-center border'>
-          <div className="h-4 w-4 bg-verde rounded-full absolute top-[172px] left-[198px]"></div>
-          <Image
-            className='rounded-full'
-            src="/img/perfil.jpg"
-            alt="Perfil photo"
-            width={150}
-            height={150}
-          />
-          <div className="flex flex-col gap-3">
-            <h3 className='text-lg font-[550] mt-8'>Esneider Ballesta</h3>
-            <p className='text-[15px] leading-6 text-text-secondary whitespace-nowrap'>Ingeniería De Sistemas</p>
-          </div>
-        </div>
+        <aside className=" flex flex-col w-[305px] h-full bg-white text-text-primary items-center">
+            <div className='relative flex flex-col h-fit mx-[74px] mt-[50px] mb-[60px] text-center items-center'>
+                <div className="h-4 w-4 bg-verde rounded-full absolute bottom-28 left-[125px]"></div>
+                <Image
+                    className='rounded-full'
+                    src="/img/perfil.jpg"
+                    alt="Perfil photo"
+                    width={150}
+                    height={150}
+                />
+                <div className="flex flex-col gap-3">
+                    <h3 className='text-lg font-[550] mt-8'>Esneider Ballesta</h3>
+                    <p className='text-[15px] leading-6 text-text-secondary whitespace-nowrap'>Ingeniería De Sistemas</p>
+                </div>
+            </div>
 
-        <hr className="w-[220px] mb-6" />
+            <hr className="w-[220px] mb-6" />
 
-        <div className="flex flex-col gap-2 w-[220px] text-[15px]">
-          <div className="flex justify-between">
-            <h4>Age:</h4>
-            <h4>24</h4>
-          </div>
-          <div className="flex justify-between">
-            <h4>Residence:</h4>
-            <h4>BD</h4>
-          </div>
-          <div className="flex justify-between">
-            <h4>Freelance:</h4>
-            <h4 className="text-[#7EB942]">Available</h4>
-          </div>
-          <div className="flex justify-between">
-            <h4>Address:</h4>
-            <h4>Apartadó, Ant</h4>
-          </div>
-        </div>
+            <div className="flex flex-col gap-2 w-[220px] text-[15px]">
+                <div className="flex justify-between">
+                    <h4>Age:</h4>
+                    <h4>24</h4>
+                </div>
+                <div className="flex justify-between">
+                    <h4>Residence:</h4>
+                    <h4>CO</h4>
+                </div>
+                <div className="flex justify-between">
+                    <h4>Freelance:</h4>
+                    <h4 className="text-[#7EB942]">Available</h4>
+                </div>
+                <div className="flex justify-between">
+                    <h4>Address:</h4>
+                    <h4>Apartadó, Ant</h4>
+                </div>
+            </div>
 
-        <hr className="w-[220px] my-6" />
+            <hr className="w-[220px] my-6" />
 
-        <div className='w-[220px]'>
-          <h3 className="text-lg font-[550]">Languajes</h3>
-          <div className="flex justify-between">
-            <h4>Spanish:</h4>
-            <h4>100%</h4>
-          </div>
-          <div className="border border-amarillo rounded-full h-[6px] p-[1px]">
-            <div className="bg-amarillo h-[2px] w-full rounded-full"></div>
-          </div>
-          <div className="flex justify-between">
-            <h4>English:</h4>
-            <h4>50%</h4>
-          </div>
-          <div className="border border-amarillo rounded-full h-[6px] p-[1px]">
-            <div className="bg-amarillo h-[2px] w-1/2 rounded-full"></div>
-          </div>
-          <div className="flex justify-between">
-            <h4>Portugues:</h4>
-            <h4>0%</h4>
-          </div>
-          <div className="border border-amarillo rounded-full h-[6px] p-[1px]">
-            <div className="bg-amarillo h-[2px] w-0 rounded-full"></div>
-          </div>
-        </div>
+            <div className='w-[220px] space-y-[9px]'>
+                <h3 className="text-lg font-[550]">Languajes</h3>
+                <ProgressBar name='Spanish' progress="30" />
+                <ProgressBar name='English' progress="50" />
+            </div>
 
-        <hr className="w-[220px] my-6" />
+            <hr className="w-[220px] my-6" />
 
-        <div className="flex flex-col gap-1 w-[220px]">
-          <h3 className="text-lg font-[550] mb-2">Extra Skills</h3>
-          <div className="flex gap-4">
-            <TbLayersIntersect className='text-amarillo text-xl scale-x-[-1]' />
-            <p>SpringBoot</p>
-          </div>
-          <div className="flex gap-4">
-            <TbLayersIntersect className='text-amarillo text-xl scale-x-[-1]' />
-            <p>React, Nextjs</p>
-          </div>
-          <div className="flex gap-4">
-            <TbLayersIntersect className='text-amarillo text-xl scale-x-[-1]' />
-            <p>Git, Github</p>
-          </div>
-          <div className="flex gap-4">
-            <TbLayersIntersect className='text-amarillo text-xl scale-x-[-1]' />
-            <p>Logica de Programación</p>
-          </div>
+            <div className='w-[220px] space-y-[9px]'>
+                <h3 className="text-lg font-[550]">Programming Languages</h3>
+                <ProgressBar name='Java' progress="80" />
+                <ProgressBar name='Python' progress="40" />
+                <ProgressBar name='Html' progress="70" />
+                <ProgressBar name='CSS' progress="50" />
+            </div>
+            
+            <hr className="w-[220px] my-6" />
 
-        </div>
+            <SkillsSection skills={skillsList} />
 
-      </aside>
+        </aside>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image"
 import { Arimo, Inter, Oxanium } from 'next/font/google'
-
-import {BsFacebook, BsGithub, BsLinkedin, BsTwitter, BsYoutube } from 'react-icons/bs'
-import {BiLogoInstagramAlt} from 'react-icons/bi'
-import {FaLinkedinIn, FaFacebookF} from 'react-icons/fa'
 import { LefAside } from "@/components/LeftAside"
+import { RightAside } from "@/components/RigthAside"
+import { BsArrowRightShort, BsCodeSlash } from 'react-icons/bs'
+import { BiCodeBlock } from 'react-icons/bi'
+import { FaLaptopCode, FaFileCode } from 'react-icons/fa'
+import { PiDesktop } from 'react-icons/pi'
+import { CiDesktop } from 'react-icons/ci'
+import { LiaLaptopCodeSolid } from 'react-icons/lia'
+import { TbDeviceImacCode } from 'react-icons/tb'
 
 const inter = Inter({ subsets: ['latin'] })
 const arimo = Arimo({ subsets: ['latin'] })
@@ -13,14 +17,12 @@ const oxanium = Oxanium({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`${oxanium.className} flex flex-row h-screen justify-center bg-[#F0F0F6] gap-4  border border-red-500`}
+      className={`${oxanium.className} flex flex-row h-screen justify-center bg-[#F0F0F6] gap-4`}
     >
       <LefAside />
-      <div className='w-[1006px] h-full border border-orange-700'></div>
-      <aside className='flex flex-col gap-5 items-center w-[97px] h-full bg-white border border-blue-700 pt-[73px]'>
-
-        
-      </aside>
+      <div className='flex flex-col gap-11 w-[1006px] h-full items-center border border-orange-700 overflow-y-auto'>
+      </div> 
+      <RightAside />
 
     </main>
   )
